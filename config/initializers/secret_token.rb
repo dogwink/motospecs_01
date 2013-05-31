0bab41764ae742e8a51fc58394792263f83cbfb6
Allen Ee <allen.ee@moto-research.com>
2013-05-31 17:05:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Motospecs01::Application.config.secret_token = 'fce903df3c55efe28a2785039de90e2958d0bd235c9aa1cfdee7114266b3435cc94938549595a93b488b4d3afda952148c6bc01dffe06a96018bf686dfa98806'
